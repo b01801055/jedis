8bdc463acb6c518aa33ce3ed700e604f502bfdfe
Ron <a3300552@hpicorp.com.tw>
2020-08-20 18:20:54 +0800

    Update JedisUtil.java
    ok

--- src/main/java/com/ron/jedis/util/JedisUtil.java
+++ src/main/java/com/ron/jedis/util/JedisUtil.java
@@ -3,7 +3,9 @@ package com.ron.jedis.util;
 import redis.clients.jedis.Jedis;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class JedisUtil {
     Jedis jedis = new Jedis("127.0.0.1", 6379);
@@ -21,7 +23,7 @@ public class JedisUtil {
         jedis.hmset(UID, map);
     }
 
-    public Map<String, String> findByUid(String UID) {
+    public Map<String, String> findTag_IdTypeByUid(String UID) {
         return jedis.hgetAll(UID);
     }
 
@@ -39,5 +41,39 @@ public class JedisUtil {
         Map<String, Double> map = new HashMap<>();
         map.put(UID,TIMESTAMP);
         jedis.zadd(TAG_ID, map);
+    }
+
+    public double findTag_IdTimeStampByUid(String UID) {
+        saveTag_IdTimeStampUid();
+        String TAG_ID = null;
+        return jedis.zscore(TAG_ID,UID);
+    }
+
+    public Set<String> findUidByTag_Id(String TAG_ID,double min,double max) {
+        return jedis.zrangeByScore(TAG_ID,min,max);
+    }
+
+    public long countByTag_Id(String TAG_ID,double min,double max) {
+        return jedis.zcount(TAG_ID,min,max);
+    }
+
+    public void saveCountUnion(String TAG_ID) {
+        Map<String,Double> map = new HashMap<>();
+        String UID = null;
+        jedis.zadd(UID,map);
+        jedis.zadd(UID,map);
+        jedis.zadd(UID,map);
+        long TAG_COUNT = jedis.llen(UID);
+        jedis.zunionstore(TAG_ID, String.valueOf(TAG_COUNT),UID,UID,UID);
+    }
+
+    public void saveCountInter(String TAG_ID) {
+        Map<String,Double> map = new HashMap<>();
+        String UID = null;
+        jedis.zadd(UID,map);
+        jedis.zadd(UID,map);
+        jedis.zadd(UID,map);
+        long TAG_COUNT = jedis.llen(UID);
+        jedis.zinterstore(TAG_ID, String.valueOf(TAG_COUNT),UID,UID,UID);
     }
 }
